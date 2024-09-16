b602a6166d53cdc2102bc5fa65240b7fbb5dd862
Tal Liron <tal.liron@gmail.com>
2024-09-16 11:03:12 -0500

    Initial AWX workflow

--- examples/ansible/tko/library/tko.py
+++ examples/ansible/tko/library/tko.py
@@ -8,17 +8,17 @@ import tko
 
 
 def run_module():
-    module = AnsibleModule(argument_spec=dict(
-        offset=dict(type='int', default=0),
-        max_count=dict(type='int', default=100),
-    ))
+  module = AnsibleModule(argument_spec=dict(
+    offset=dict(type='int', default=0),
+    max_count=dict(type='int', default=100),
+  ))
 
-    with tko.Client(host='tko-data:50050') as client:
-        sites = client.list_sites(offset=module.params['offset'], max_count=module.params['max_count'])
-        sites = [MessageToString(v) for v in sites]
+  with tko.Client(host='tko-data:50050') as client:
+    sites = client.list_sites(offset=module.params['offset'], max_count=module.params['max_count'])
+    sites = [MessageToString(v) for v in sites]
 
-    module.exit_json(changed=False, sites=sites)
+  module.exit_json(changed=False, sites=sites)
 
 
 if __name__ == '__main__':
-    run_module()
+  run_module()
