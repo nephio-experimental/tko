987e2799c8848a08db0c5529d0ecee5e50881d48
Tal Liron <tal.liron@gmail.com>
2024-09-25 20:09:48 -0500

    AWX: filter sites

--- examples/ansible/foobar/inventory-sites.py
+++ examples/ansible/foobar/inventory-sites.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-import tko, tko.ansible
+import tko, tko.ansible, os
 
+
+metadata_patterns = os.getenv('TKO_METADATA')
+if metadata_patterns:
+  metadata_patterns = tko.yaml.load(metadata_patterns)
 
 inventory = tko.ansible.Inventory()
 
 with tko.Client(host=tko.ansible.DATA_HOST) as client:
-  for listed_site in client.list_sites(offset=0, max_count=tko.ansible.MAX_COUNT):
+  for listed_site in client.list_sites(offset=0, max_count=tko.ansible.MAX_COUNT, metadata_patterns=metadata_patterns):
     site = client.get_site(listed_site.siteId)
     inventory.add(site.siteId, {
       'template_id': site.templateId,
